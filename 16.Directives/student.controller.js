51f6cf59cb94c27cc661b39066080d835a280199
Tales Bitelo Viegas <selatotal@gmail.com>
2016-10-28 14:19:48 -0200

    16 - Directives

---
+++ 16.Directives/student.controller.js
@@ -0,0 +1,13 @@
+mainApp.controller('StudentController', function($scope) {
+
+	$scope.Tales = {
+		name: "Tales Viegas",
+		rollno: 1
+	};
+
+	$scope.Cavalini = {
+		name: "Cavalini",
+		rollno: 2
+	}
+
+});
---
+++ 16.Directives/student.directive.js
@@ -0,0 +1,23 @@
+mainApp.directive('student', function() {
+
+	var directive = {
+		restrict: 'E',
+		template: "Student: <b>{{student.name}}</b>, Roll No: <b>{{student.rollno}}</b>",
+		scope: {
+			student: "=name"
+		},
+
+		compile: function(element, attributes){
+			element.css("border", "1px solid #cccccc");
+
+			var linkFunction = function($scope, element, attributes) {
+				element.html("Student: <b>" + $scope.student.name+"</b>, Roll No: <b>" + $scope.student.rollno + "</b><br/>");
+				element.css("background-color", "#ff00ff");
+			}
+			return linkFunction;
+		}
+	};
+
+	return directive;
+
+})
