2bd783e6a68968c9bf5662d8bee2c0639a8f1831
Tales Bitelo Viegas <selatotal@gmail.com>
2016-10-28 11:14:00 -0200

    10 - Includes

---
+++ 10.Includes/student.controller.js
@@ -0,0 +1,20 @@
+mainApp.controller('studentController', function($scope){
+
+	$scope.student = {
+		firstName: "Tales",
+		lastName: "Viegas",
+		fees: 500,
+
+		subjects: [
+			{ name: 'Física', marks: 70 },
+			{ name: 'Química', marks: 80 },
+			{ name: 'Matemática', marks: 65 }
+		],
+
+		fullName: function(){
+			var studentObject = $scope.student;
+			return studentObject.firstName + ' ' + studentObject.lastName;
+		}
+	}
+
+});
